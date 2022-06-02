3171d48af5789af69038bda97291665efeb4912c
Thiago <thiago.michel@institutoivoti.com.br>
2022-06-02 15:26:42 -0300

    Login funcional

--- src/php/autenticacao.php
+++ src/php/autenticacao.php
@@ -2,21 +2,11 @@
 	// conexão
 	include_once "conexao.php";
 
-	$conn = mysqli_connect($localhost, $user, $password, $banco);
-
-	if (!$conn)
-	{
-		echo  "<script>alert('Não foi possível conectar ao Banco de Dados. Usuário ou Senha inválidos!');</script>";
-		header('Location: login.html');
-	}
-
-	$user = $_POST['usuario'];
-	$password = $_POST['senha'];
-	$tipo = $_POST['options'];
+	$email = $_POST['email'];
+	$senha = $_POST['senha'];
 	
-	$sql = "SELECT * FROM USUARIOS WHERE (Email='$user') AND (Senha='$password')";
+	$sql = "SELECT * FROM fornecedor WHERE (email='$email') AND (senha='$senha')";
 
-	
 	$result = mysqli_query($conn, $sql);
 	$row = mysqli_num_rows($result);
 
@@ -26,7 +16,7 @@
 		session_start();
 		$_SESSION['usuario'] = $user;
 		$_SESSION['senha'] = $password;
-		header('Location: login.php');
+		header('Location: ../sobre.html');
 	}
 	else
 	{
