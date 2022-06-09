5138956fd7e9618c1b2494fefe2a008967564b4f
MichelThiago3211 <thiago.michel@institutoivoti.com.br>
2022-06-09 14:51:30 -0300

    checkpoint

--- src/php/adicionar_fornecedor.php
+++ src/php/adicionar_fornecedor.php
@@ -17,6 +17,15 @@
     $numero = $_POST['numero'];
     $cep = str_replace("-", "", $_POST['cep']);
     $cidade = $_POST['cidade'];
+    $estado = $_POST['estado'];
+    echo $estado;
+    $UF;
+
+    switch(){
+        case '':
+
+
+    }
 
 	$diretorio  = "../images/";
     $temArquivo = $_FILES["imagem"]['name'] != '';
@@ -25,9 +34,17 @@
 
     $sql = "SELECT id_municipio FROM municipio WHERE (nome='$cidade')";
     $result = mysqli_query($conn, $sql);
-    $idMunicipio = 1;
+    $idMunicipio = mysqli_fetch_array($result, MYSQLI_NUM)[0];  
+    
+    if(is_null($idMunicipio)){
 
-    $sql = "INSERT INTO fornecedor(complemento, numero, rua, cep, nome, telefone, email, senha, ativo, cnp, tipo, id_municipio". ($temArquivo? ', imagem' : '') . ") VALUES ('$complemento', $numero, '$rua', $cep, '$nomeCompleto', '$telefone', '$email', '$senha', 0, '$cnp', '$tipo', $idMunicipio" . ($temArquivo? ", '".$logo."'" : '') . ");";
+        $sql = "INSERT INTO MUNICIPIO(NOME,ESTADO) VALUES ('$idMunicipio', '')"
+
+    }
+
+
+
+    $sql = "INSERT INTO fornecedor(complemento, numero, rua, cep, nome, telefone, email, senha, ativo, cnp, tipo, id_municipio". ($temArquivo? ', imagem' : '') . ") VALUES ('$complemento', $numero, '$rua', $cep, '$nomeCompleto', '$telefone', '$email', '$senha', 0, '$cnp', '$tipo', $idMunicipio " . ($temArquivo? ", '".$logo."'" : '') . ");";
     $result = mysqli_query($conn, $sql);
    
     if (!$result) {
