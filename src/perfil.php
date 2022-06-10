2c7148f90d0acd1a266f3c67c0bc458e05b4968d
Nicolas Spaniol <nicolas.spaniol@institutoivoti.com.br>
2022-06-10 15:48:46 -0300

    início perfil

---
+++ src/perfil.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Catálogo</title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/perfil.css">
+
+    <!-- Fontes -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro&family=Ubuntu:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <?php include "_header.php"; ?>
+
+    <main>
+    </main>
+</body>
+</html>
