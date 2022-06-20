95b4122612bfa7467a2fb5d0a0140d8a1c3b3082
Nicolas Spaniol <nicolas.spaniol@institutoivoti.com.br>
2022-06-20 17:53:06 -0300

    deleta a imagem da peça do diretório

--- src/php/deletar_peca.php
+++ src/php/deletar_peca.php
@@ -5,6 +5,15 @@ include_once "sessao.php";
 $id = $_GET["id"] ?? false;
 
 if ($id) {
+    $stm = $conexao->prepare("SELECT imagem FROM imagem_peca WHERE id_peca = ?");
+    $stm->bind_param("i", $id);
+    $stm->execute();
+    $res = $stm->get_result();
+    
+    while ($imagem = $res->fetch_array()) {
+        unlink("../" . $imagem[0]);
+    }
+
     $stm = $conexao->prepare("DELETE FROM imagem_peca WHERE id_peca = ?");
     $stm->bind_param("i", $id);
     $stm->execute();
