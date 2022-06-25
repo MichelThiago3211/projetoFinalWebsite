7c69b83f4fed48b31c29e0fe7bcb9331f151e916
Nicolas Spaniol <nicolas.spaniol@institutoivoti.com.br>
2022-06-25 16:50:16 -0300

    conserta o título da página do perfil

--- src/perfil.php
+++ src/perfil.php
@@ -54,7 +54,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Catálogo</title>
+    <title><?= $fornecedor->nome ?></title>
 
     <!-- JavaScript -->
     <script src="js/perfil.js" defer></script>
